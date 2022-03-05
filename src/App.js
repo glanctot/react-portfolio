4fbcd775e2fc6ba9583a37f3adfb27cd932b8a1a
glanctot <gregory.lanctot28@gmail.com>
2022-03-05 01:12:28 -0600

    help push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/home/home';
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Nav from './components/nav/nav';
+import { Switch } from 'react-router-dom';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav></Nav>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/about">
+              <About></About>
+            </Route>
+            <Route exact path="/projects">
+              <Projects></Projects>
+            </Route>
+            <Route exact path="/contact">
+              <Contact></Contact>
+            </Route>
+            <Route exact path="/resume">
+              <Resume></Resume>
+            </Route>
+          </Switch>
+          
+        </div>
+        
+      </Router>
     </div>
   );
 }
