f4d28c18e10bea8973c40df5de9f03028020b487
glanctot <gregory.lanctot28@gmail.com>
2022-03-07 11:59:54 -0600

    push

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import './contact.scss'
 
 function Contact() {
     return (
-        <h1>Contact</h1>
+        <div className="contact">
+            <h1>Contact Me</h1>
+            <form className="form">
+                <label htmlFor="name">Name:</label>
+                <input type="text"></input>
+                <label htmlFor="email">Email:</label>
+                <input type="text"></input>
+                <label htmlFor="message">Message:</label>
+                <textarea type="text"></textarea>
+                <button>Submit</button>    
+            </form>
+        </div>
     )
 }
 
